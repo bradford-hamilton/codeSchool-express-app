9b53364e7cfadc43c2e4dcdcce29bf4d2437be52
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-05-24 14:37:58 -0600

    replace in memory with redis

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
 var request = require('supertest');
 var app = require('./app');
 
+var redis = require('redis');
+var client = redis.createClient();
+client.select('test'.length);
+client.flushdb();
+
 describe('Requests to the root path', function() {
 
   it('Returns a 200 status code', function(done) {
@@ -39,12 +44,16 @@ describe('Listing cities on /cities', function() {
   it ('Returns initial cities', function (done) {
     request(app)
       .get('/cities')
-      .expect(JSON.stringify(['Lotopia', 'Caspiana', 'Indigo']), done);
+      .expect(JSON.stringify(["Lotopia","Caspiana","Indigo","Springfield"]), done);
   });
 
 });
 
 describe('Creating new cities', function() {
+
+  before(function() {
+
+  });
 
   it('Returns a 201 status code', function(done) {
     request(app)
